ac2cc3ad21e8bb043aae2d30b30314295861eccc
msv96 <msrini1100@gmail.com>
2021-08-18 20:15:47 +0530

    Update EditProduct.js

--- src/EditProduct.js
+++ src/EditProduct.js
@@ -23,18 +23,18 @@ function EditProduct(props) {
     el.preventDefault();
     let productData = { userName, position, office, age, startDate, salary };
     productContext.productList[props.match.params.id - 1] = productData;
-    productContext.setProductList([...productContext.productList, productData]);
+    productContext.setProductList([...productContext.productList]);
     history.push("/product");
   };
   return (
     <div>
       <div className="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 className="h3 mb-0 text-gray-800">Create User</h1>
+        <h1 className="h3 mb-0 text-gray-800">Edit Product</h1>
       </div>
       <form onSubmit={handleSubmit}>
         <div className="container">
           <div className="row">
-          <div className="col-lg-6">
+            <div className="col-lg-6">
               <label>Username</label>
               <input
                 type="text"
@@ -109,7 +109,7 @@ function EditProduct(props) {
             <div className="col-lg-12">
               <input
                 type="submit"
-                value="Submit"
+                value="Update"
                 className="btn btn-primary mt-3"
               />
             </div>
