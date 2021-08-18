08132888b7e9643c7b6fbe22bd6593187e41dbff
msv96 <msrini1100@gmail.com>
2021-08-18 20:24:29 +0530

    Update CreateProduct.js

--- src/CreateProduct.js
+++ src/CreateProduct.js
@@ -21,7 +21,7 @@ function CreateProduct() {
   return (
     <div>
       <div className="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 className="h3 mb-0 text-gray-800">Create User</h1>
+        <h1 className="h3 mb-0 text-gray-800">Create Product</h1>
       </div>
       <form onSubmit={handleSubmit}>
         <div className="container">
